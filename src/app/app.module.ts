6845ca2398bf22b36d5afb23d4dfe9bb9213e698
Juanse <1497juan@gmail.com>
2019-11-18 08:26:36 -0500

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroclienteComponent } from './componentes/registrocliente/registrocliente.component';
-import { ListaClientesComponent } from './componentes/lista-clientes/lista-clientes.component';
+import { ListaClientesComponent } from './componentes/menu/lista-clientes/lista-clientes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { IngresarComponent } from './componentes/ingresar/ingresar.component';
 import { ServiciosService } from './servicios/servicios.service'
 import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatDialogRef, MatDialog } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
-import { RegistroHijoComponent } from './componentes/registro-hijo/registro-hijo.component';
-import { RegistroMembresiaComponent } from './componentes/registro-membresia/registro-membresia.component';
-import { RegistroProveedorComponent } from './componentes/registro-proveedor/registro-proveedor.component';
-import { RegistroProductoComponent } from './componentes/registro-producto/registro-producto.component';
-import { ListaHijosComponent } from './componentes/lista-hijos/lista-hijos.component';
-import { ListaProveedoresComponent } from './componentes/lista-proveedores/lista-proveedores.component';
-import { ListaProductosComponent } from './componentes/lista-productos/lista-productos.component';
-import { ListaHijosClienteComponent } from './componentes/lista-hijos-cliente/lista-hijos-cliente.component';
-import { ListaProductosProveedorComponent } from './componentes/lista-productos-proveedor/lista-productos-proveedor.component';
+import { RegistroHijoComponent } from './componentes/menu/registro-hijo/registro-hijo.component';
+import { RegistroMembresiaComponent } from './componentes/menu/registro-membresia/registro-membresia.component';
+import { RegistroProveedorComponent } from './componentes/menu/registro-proveedor/registro-proveedor.component';
+import { RegistroProductoComponent } from './componentes/menu/registro-producto/registro-producto.component';
+import { ListaHijosComponent } from './componentes/menu/lista-hijos/lista-hijos.component';
+import { ListaProveedoresComponent } from './componentes/menu/lista-proveedores/lista-proveedores.component';
+import { ListaProductosComponent } from './componentes/menu/lista-productos/lista-productos.component';
+import { ListaHijosClienteComponent } from './componentes/menu/lista-hijos-cliente/lista-hijos-cliente.component';
+import { ListaProductosProveedorComponent } from './componentes/menu/lista-productos-proveedor/lista-productos-proveedor.component';
 
 
 @NgModule({
--- src/app/componentes/menu/lista-clientes/lista-clientes.component.ts
+++ src/app/componentes/menu/lista-clientes/lista-clientes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiciosService } from 'src/app/servicios/servicios.service';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatDialog } from '@angular/material';
 import { cliente } from 'src/app/modelos/cliente';
+import { hijo } from 'src/app/modelos/hijo';
 
 @Component({
   selector: 'app-lista-clientes',
@@ -9,28 +10,35 @@ import { cliente } from 'src/app/modelos/cliente';
   styleUrls: ['./lista-clientes.component.css']
 })
 export class ListaClientesComponent implements OnInit {
+  hijo: hijo
   clientes: any;
   columsToDisplay = ['id_cliente', 'nombre', 'apellido', 'correo', 'hijos', 'dhijos', 'opciones'];
   dataSource: MatTableDataSource<cliente>;
 
   constructor(
-     private serviciosService: ServiciosService
+    private serviciosService: ServiciosService,
   ) { }
 
   ngOnInit() {
     this.obtenerClientes();
   }
 
-obtenerClientes(){
+  obtenerClientes() {
 
-  this.serviciosService.listarClientes().subscribe(
-    cliente => {
-      this.clientes = cliente;
-      this.dataSource = new MatTableDataSource<cliente>(this.clientes);
-      console.log(this.clientes)
-    }   
-  );
-}
+    this.serviciosService.listarClientes().subscribe(
+      cliente => {
+        this.clientes = cliente;
+        this.dataSource = new MatTableDataSource<cliente>(this.clientes);
+        console.log(this.clientes)
+      }
+    );
+  }
+
+  listahijo(hijo) {
+    this.hijo = hijo;
+    console.log(this.hijo)
+
+  }
 
 
 }
